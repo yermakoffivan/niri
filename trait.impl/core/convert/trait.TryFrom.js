8868048b2ac3a879198cd2a34e98f62b092fa353
YaLTeR <YaLTeR@users.noreply.github.com>
2026-09-24 17:08:53 +0000

    deploy: c3e25664b36f820ae7d4ec9eecfbdd26bd1db088

---
+++ trait.impl/core/convert/trait.TryFrom.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["niri_ipc",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.98.1/std/primitive.u8.html\">u8</a>&gt; for <a class=\"enum\" href=\"niri_ipc/enum.MaxBpc.html\" title=\"enum niri_ipc::MaxBpc\">MaxBpc</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[370]}
